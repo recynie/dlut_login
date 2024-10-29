3bc3d67bea8048d495816e581052ea7f2c653d1b
recynie <littledai_oursky@163.com>
2024-10-29 11:24:30 +0800

    change target template&shell path

--- bash_gen.py
+++ bash_gen.py
@@ -9,8 +9,8 @@ with open('info.json','r') as f:
 ID = d['id']
 PASSWORD = d['password']
 
-TEMPLATE='dlut_login/template.sh'
-TARGET=f'dlut_login/login.sh'
+TEMPLATE='template.sh'
+TARGET=f'login.sh'
 
 e = '010001'
 m = '94dd2a8675fb779e6b9f7103698634cd400f27a154afa67af6166a43fc26417222a79506d34cacc7641946abda1785b7acf9910ad6a0978c91ec84d40b71d2891379af19ffb333e7517e390bd26ac312fe940c340466b4a5d4af1d65c3b5944078f96a1a51a5a53e4bc302818b7c9f63c4a1b07bd7d874cef1c3d4b2f5eb7871'
